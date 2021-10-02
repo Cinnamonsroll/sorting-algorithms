93edda3c558520f817e80e8b44c91b5424905ea3
voltrexmaster <mohammadkeyvanzade94@gmail.com>
2021-10-02 03:46:57 -0700

    refactor: improve sorting implementations
    Improved the sorting implementations by refactoring some key elements and adding type checks.

--- stoogeSort.js
+++ stoogeSort.js
@@ -1,5 +1,13 @@
 function stoogeSort(array, start, end) {
+  if (!Array.isArray(array))
+    throw new TypeError("The 'array' argument must be an array.");
+  if (!Number.isInteger(start))
+    throw new TypeError("The 'start' argument must be an integer.");
+  if (!Number.isInteger(end))
+    throw new TypeError("The 'end' argument must be an integer.");
+
   let pivot;
+
   if (end - start + 1 > 2) {
     pivot = ~~((end - start + 1) / 3);
     stoogeSort(array, start, end - pivot);
@@ -7,8 +15,8 @@ function stoogeSort(array, start, end) {
     stoogeSort(array, start, end - pivot);
   }
 
-  if (array[start] > array[end]) {
+  if (array[start] > array[end])
     [array[start], array[end]] = [array[end], array[start]];
-  }
+
   return array
 }
